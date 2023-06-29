b94f5095b16127b554ae0eb945f23fb2f15d166b
MohammedSB <MohammedSalimAB@outlook.com>
2023-06-29 23:16:05 +0300

    changed db to postgres

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../../components/Navbar/Navbar";
-import "./MainPage.css"
+import "./MainPage.scss"
 
 export default function MainPage() {
     return(
@@ -9,6 +9,34 @@ export default function MainPage() {
             <p className="info-title">اهلآ بك في راوية</p>    
             <p>موقع لكتابة و نشر القصص و الروايات العربية</p>    
         </div>
+
+        {/* What to do o+n the website container */}
+        <div className="wtd-container">
+            <div className="wtd-box-container">
+            
+                <div className="wtd-box">
+                <div className="wtd-box-title">
+                    w
+                </div>
+                </div>
+
+            </div>
+            <div className="wtd-box-container">
+              
+                <div className="wtd-box">
+
+                </div>
+
+            </div>
+            <div className="wtd-box-container">
+
+                <div className="wtd-box">
+
+                </div>
+
+            </div>
+        </div>
+
         </>
     )
 }
